e9aaf7615aa611b1d23178782e3c77019ad51fce
kazuki matsumoto <mormorbump@gmail.com>
2019-09-04 19:25:57 +0900

    機能fix

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,9 +3,13 @@ class StoreController < ApplicationController
   skip_before_action :authorize
   
   def index
-    @count = current_counter
-    # orderメソッドで商品名の降順で表示
-    @products = Product.order(:title)
-    @cart = current_cart
+    if params[:set_locale]
+      redirect_to root_path(locale: params[:set_locale])
+    else
+      @count = current_counter
+      # orderメソッドで商品名の降順で表示
+      @products = Product.order(:title)
+      @cart = current_cart
+    end
   end
 end
